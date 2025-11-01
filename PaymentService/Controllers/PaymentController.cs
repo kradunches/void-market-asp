d13e292b8bdc6243ec903205fb1e90a8ba310b22
Ivan Monichev <monichevid@baltech.ru>
2025-11-01 22:20:41 +0300

    feat: update mode for publisher

--- PaymentService/Controllers/PaymentController.cs
+++ PaymentService/Controllers/PaymentController.cs
@@ -1,6 +1,5 @@
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
-using PaymentService.Controller;
 using PaymentService.Dto;
 using Shared.Contracts;
 
@@ -11,29 +10,24 @@ namespace PaymentService.Controllers;
 public class PaymentController : ControllerBase
 {
     private readonly IPublishEndpoint _publishEndpoint;
-    private readonly IOrderServiceClient _orderServiceClient;
 
-    public PaymentController(IPublishEndpoint publishEndpoint, IOrderServiceClient orderServiceClient)
+    public PaymentController(IPublishEndpoint publishEndpoint)
     {
         _publishEndpoint = publishEndpoint;
-        _orderServiceClient = orderServiceClient;
     }
 
     [HttpPost("{id:long}/status")]
-    public async Task<IActionResult> UpdateOrderStatus(long id, [FromBody] UpdateOrderStatusRequestDto request)
+    public IActionResult UpdateOrderStatus(long id, [FromBody] UpdateOrderStatusRequestDto request)
     {
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
-        
-        if (!await _orderServiceClient.OrderExistsAsync(id))
-            return NotFound($"Order with ID {id} not found");
 
-        await _publishEndpoint.Publish(new OrderStatusUpdated
+        _ = _publishEndpoint.Publish(new OrderStatusUpdated
         {
             OrderId = id,
             Status = request.Status.ToString().ToLowerInvariant()
         });
 
-        return Accepted(new { message = "status update published" });
+        return Accepted();
     }
 }
